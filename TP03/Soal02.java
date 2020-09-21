f954f5cb0d2f9793eb31af72021805e6e467d4aa
Al Hadid Defry Rinandi <alhadiddefri@student.telkomuniversity.ac.id>
2020-09-21 14:54:03 +0700

    Soal02.java

---
+++ TP03/Soal02.java
@@ -0,0 +1,8 @@
+public class Soal02{
+   public static void main(String[]args){ 
+      int umur = 18;
+      String name = "Al Hadid Defry Rinandi";
+      System.out.println("Halo, " + name);
+      System.out.println("Ternyata usiamu baru " + umur + " tahun ya!");
+   }
+}
